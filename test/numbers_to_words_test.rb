1499be71bba12f6f8742bf8fb66009f2932dbef5
Karel Minarik <karmi@karmi.cz>
2009-08-22 22:24:34 +0200

    [STEP 1] Specifying API for number 1
    * We don't care about implementation now
    * We care about the API

--- test/numbers_to_words_test.rb
+++ test/numbers_to_words_test.rb
@@ -1,5 +1,17 @@
 require 'test/unit'
+
+begin
+  require 'redgreen'
+rescue LoadError
+  puts "[!] Install redgreen gem for better test output ($ sudo gem install redgreen)"
+end unless ENV["TM_FILEPATH"]
+
 require File.join( File.dirname(__FILE__), '..', 'numbers_to_words' )
 
 class NumbersToWordsTest < Test::Unit::TestCase
+
+  def test_1_should_return_one
+    assert_equal 'one', 1.to_words
+  end  
+
 end
