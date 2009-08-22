8f64bd38e2aeeba7bfa6f6fc74074e5e8442aeeb
Karel Minarik <karmi@karmi.cz>
2009-08-22 20:53:09 +0200

    [STEP 11] Implementing logic for translating numbers in range from 20 to 99. FINAL STEP
    * The concrete Ruby code is _not_ important, here
    * The implementation could differ, use different approaches, etc
    * The code is written so it's neither too long or too short and is (hopefully) readable

--- numbers_to_words.rb
+++ numbers_to_words.rb
@@ -1,18 +1,30 @@
 module NumbersToWords
 
-  UNITS = %w{zero one two three four five six seven eight nine ten}
-  TEENS = %w{~ eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen}
-
   module InstanceMethods
 
     def to_words
       case self
-        when 0..10
-          UNITS[self]
-        when 11..19
-          TEENS[self.modulo(10)]
+      when 0..10
+        Translations::UNITS[self]
+      when 11..19
+        Translations::TEENS[self.modulo(10)]
+      when 20..99
+        unit = self.modulo(10).zero? ? nil : Translations::UNITS[self.modulo(10)]
+        [ Translations::TENS[self/10], unit ].compact.join(' ')
+      when 100..999
+        hundred = Translations::UNITS[self/100]
+        rest    = self.modulo(100).zero? ? nil : self.modulo(100).to_words
+        [ hundred, Translations::HUNDRED, rest ].compact.join(' ')
       end
     end
+
+  end
+
+  module Translations
+    UNITS    = %w{zero one two three four five six seven eight nine ten}
+    TEENS    = %w{~ eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen}
+    TENS     = %w{~ ten twenty thirty fourty fifty sixty seventy eighty ninety}
+    HUNDRED  = 'hundred'
   end
 
 end
