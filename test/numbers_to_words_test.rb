453fe2243f987c21efd8ec3ea5b7f6dc928d95c9
Karel Minarik <karmi@karmi.cz>
2009-08-22 20:34:16 +0200

    [STEP 5] Added specification for translating numbers 0 to 9
    * And the tests are RED

--- test/numbers_to_words_test.rb
+++ test/numbers_to_words_test.rb
@@ -13,20 +13,23 @@ require File.join( File.dirname(__FILE__), '..', 'numbers_to_words' )
 
 class NumbersToWordsTest < Test::Unit::TestCase
 
-  context "Number one" do
+  context "Numbers lower then 10" do
 
     should "translate to words" do
+      assert_equal 'zero',  0.to_words
       assert_equal 'one',   1.to_words
-    end
-
-  end
-
-  context "Number two" do
-
-    should "translate to words" do
       assert_equal 'two',   2.to_words
+      assert_equal 'three', 3.to_words
+      assert_equal 'four',  4.to_words
+      assert_equal 'five',  5.to_words
+      assert_equal 'six',   6.to_words
+      assert_equal 'seven', 7.to_words
+      assert_equal 'eight', 8.to_words
+      assert_equal 'nine',  9.to_words
     end
 
   end
+
+  # ---------------------------------------------------------------------------
 
 end
