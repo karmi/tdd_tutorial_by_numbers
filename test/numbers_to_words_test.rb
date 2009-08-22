b881f58890810d6db83e8034373a7d4ecd2deca9
Karel Minarik <karmi@karmi.cz>
2009-08-22 18:55:25 +0200

    [STEP 3] Adding specification for number 2 in tests and using Shoulda vocabulary
    * Using Shoulda [http://thoughtbot.com/projects/shoulda] vocabulary for better test readability
    * Adding test for number 2, forcing us to implement some logic

--- test/numbers_to_words_test.rb
+++ test/numbers_to_words_test.rb
@@ -1,3 +1,4 @@
+require 'rubygems'
 require 'test/unit'
 
 begin
@@ -6,12 +7,26 @@ rescue LoadError
   puts "[!] Install redgreen gem for better test output ($ sudo gem install redgreen)"
 end unless ENV["TM_FILEPATH"]
 
+require 'shoulda'
+
 require File.join( File.dirname(__FILE__), '..', 'numbers_to_words' )
 
 class NumbersToWordsTest < Test::Unit::TestCase
 
-  def test_1_should_return_one
-    assert_equal 'one', 1.to_words
-  end  
+  context "Number one" do
+
+    should "translate to words" do
+      assert_equal 'one',   1.to_words
+    end
+
+  end
+
+  context "Number two" do
+
+    should "translate to words" do
+      assert_equal 'two',   2.to_words
+    end
+
+  end
 
 end
