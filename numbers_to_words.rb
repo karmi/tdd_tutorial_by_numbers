2c9f2331aa6444a48902ce15d0dbc5566db11b77
Karel Minarik <karmi@karmi.cz>
2009-08-22 20:41:39 +0200

    [STEP 8] Implementing logic for translating numbers 10 to 19

--- numbers_to_words.rb
+++ numbers_to_words.rb
@@ -1,8 +1,14 @@
 class Fixnum
 
   UNITS = %w{zero one two three four five six seven eight nine ten}
+  TEENS = %w{~ eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen}
 
   def to_words
-    UNITS[self]
+    case self
+      when 0..10
+        UNITS[self]
+      when 11..19
+        TEENS[self.modulo(10)]
+    end
   end
 end
